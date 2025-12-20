c19b4e230aa749fab05bab86e5ec4396ed69ac0a
JumpStone <hi@jumpstone4477.de>
2025-12-20 16:12:26 +0100

    Docs Changes

--- assets/js/load-header.js
+++ assets/js/load-header.js
@@ -14,6 +14,14 @@ window.addEventListener("DOMContentLoaded", () => {
         });
 });
 
+window.addEventListener("DOMContentLoaded", () => {
+    fetch("https://hmt-network.de/assets/templates/header-docs.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("header-docs-container").innerHTML = data;    
+        });
+});
+
 // Add scroll event listener for header shrink effect on mobile
 let lastScrollTop = 0;
 const header = document.querySelector('.header-nav');
